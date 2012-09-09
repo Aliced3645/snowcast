5f342dc4e130997cd121b38577b8d2d9d793a456
Shu Zhang <shu_zhang1989@yahoo.com.cn>
2012-09-09 18:47:27 -0400

    4

--- snowcast_control.c
+++ snowcast_control.c
@@ -10,21 +10,44 @@
 
 #define DEBUG
 
-
-
 #define MAX_LENGTH 256
 //define control messages here
+//cancel the allignment
+#pragma pack(push,1)
 struct Hello{
 	uint8_t commandType;
 	uint16_t udpPort;
 };
+#pragma pack(pop)
 int helloed = 0;
 
-struct SetStation{
-		uint8_t commanType;
-		uint16_t stationNUmber;
-};
+int send_hello(int sockfd, const char* clnt_udpport){
+	//convert host byte order to network order
+	uint16_t clnt_udpport_h = (uint16_t)atoi(clnt_udpport);
+	uint16_t clnt_udpport_n = htons(clnt_udpport_h);
+#ifdef DEBUG
+	printf("Port: %d\n", clnt_udpport_n);
+	printf("uint8 size: %d\n",sizeof(uint8_t));
+	printf("uint16 size: %d\n", sizeof(uint16_t));
+	//The machine might be 32-bit alligned...
+	printf("Hello struct size: %d\n", sizeof(struct Hello));
+#endif
+	struct Hello hl_msg = {(uint8_t)0, clnt_udpport_n};
+#ifdef DEBUG
+	printf("%d\n", sizeof(hl_msg));
+#endif
+	int bytes_sent = send(sockfd,(void*)&hl_msg, sizeof(struct Hello),0);
+	if(bytes_sent == -1){
+		printf("An error occured when sending a HELLO message: %s\n", strerror(errno));
+		return -1;
+	}
+	if(bytes_sent != 3){
+		printf("NOT all contents of the Hello mesasge is sent...\n");
+		return 0;
+	}
 
+	return 1;
+}
 
 int main(int argc, char** argv){
 	
@@ -47,28 +70,23 @@ int main(int argc, char** argv){
 	hints.ai_socktype = SOCK_STREAM;
 	int rv = 0;
 	if((rv = getaddrinfo(server_name,server_port,&hints, &servinfo) == -1)){
-		printf("Error in get address info: %s\n", strerror(errno));
+		printf("Error in getting address info: %s\n", strerror(errno));
 		exit(-1);
 	}
+	
 	if((sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol)) == -1){
+		close(sockfd);
 		printf("Error in initializing socket: %s\n", strerror(errno));
 		exit(-1);
 	}
-/*
+
 	if((rv = connect(sockfd,servinfo->ai_addr,servinfo->ai_addrlen)) == -1){
 		printf("%s\n",strerror(errno));
 		//If "connection refused, that is there is no server started yet
 		exit(-1);
-	}
-*/	
-	//convert host byte order to network order
-	uint16_t clnt_udpport_h = (uint16_t)atoi(clnt_udpport);
-	uint16_t clnt_udpport_n = htons(clnt_udpport_h);
-#ifdef DEBUG
-	printf("Port: %d\n", clnt_udpport_n);
-#endif
-	struct Hello hlMsg;
+	}	
 
+	send_hello(sockfd, clnt_udpport);
 	//waiting for input
 	printf("Connected to the server! Please Input your set station command.\n \
 			Usage: set <station number>\n");
@@ -76,18 +94,42 @@ int main(int argc, char** argv){
 	char input_msg[MAX_LENGTH];
 	memset(input_msg, 0, MAX_LENGTH);
 	while(1){
-		scanf("%s", input_msg);
+		fgets(input_msg,MAX_LENGTH,stdin);	
+		if((strlen(input_msg) > 0 ) && (input_msg[strlen(input_msg) - 1] == '\n'))
+				input_msg[strlen(input_msg) - 1] = '\0';
 		//decide what type of message
 		if(strncmp(input_msg,"set", 3) == 0){
-			printf("hi\n");
+			//take the channel number
+			char channel_str[MAX_LENGTH];
+			memset(channel_str, 0, MAX_LENGTH);
+			strcpy(channel_str, input_msg + 4);
+			char* last = 0;
+			int channel_num = strtol(channel_str, &last, 10);
+			if((strlen(channel_str) == 0) || (last[0] != '\0')){
+				printf("Ambiguous input! Do you mean 'set %d'? type [y] to confim or other keys to input again!\n", channel_num);
+				memset(input_msg, 0, MAX_LENGTH);
+				fgets(input_msg, MAX_LENGTH, stdin);
+				if( (strlen(input_msg) == 1) && input_msg[0] == 'y'){
+				//to send setstation package here....
+				printf("Sending setstation request...\n");
+				}
+				else
+					continue;
+			}
+			//to send setstation package here..
+			printf("Sending setstation request...\n");
+			
 		}
-		else if((strncmp(input_msg, "exit", 4) == 0) || (strncmp(input_msg, "quit", 4) == 0))
+		else if((strncmp(input_msg, "exit", 4) == 0) || (strncmp(input_msg, "quit", 4) == 0)){
+				printf("Thanks for using Snowcast!\n");
 				exit(0);
-		else
+		}
+		else{
 			printf("Cannot recognize your message! Please type again...\n");
-		
+		}
+
 		memset(input_msg, 0, MAX_LENGTH);
 	}
-
+	
 	return 0;
 }
--- snowcast_server.c
+++ snowcast_server.c
@@ -1,4 +1,106 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <memory.h>
+#include <inttypes.h>
+#include <pthread.h>
+#define DEBUG
 
-int main(){
+#define MAX_LENGTH 256
+
+struct listen_param{
+	int sockfd;	
+};
+
+#pragma pack(push,1)
+struct control_message{
+	uint8_t command_type;
+	uint16_t content;
+};
+#pragma pack(pop)
+
+void* listening_thread_func(void* args){
+	struct listen_param* lp = (struct isten_param*)args;
+	int server_sockfd = lp->sockfd;
+	printf("Server starts listening to connection...\n");
+	int rv;
+	if((rv = listen(server_sockfd, 0))  == -1){
+		close(server_sockfd);
+		printf("Listen Error: %s\n", strerror(errno));
+		exit(-1);
+	}
+
+	//accept a client socket
+	int client_sockfd = accept(server_sockfd, 0, 0);
+	if(client_sockfd == -1){
+		close(client_sockfd);
+		close(server_sockfd);
+		printf("Accept func error: %s\n", strerror(errno));
+		exit(-1);
+	}
+	
+	//Parse the received message
+	struct control_message msg;
+	int msg_length = 0;
+	while( (msg_length = recv(client_sockfd, &msg, sizeof(struct control_message), 0)) > 0){
+		uint8_t msg_type = msg.command_type;
+		if(msg_type == (uint8_t)0){
+			//respond to the hello here		
+			printf("Received a hello!\n");
+		}
+		else if(msg_type == (uint8_t)1){
+		
+		}
+		else{
+
+		}
+	}
+}
+
+int main(int argc, char** argv){
+	
+	if(argc < 2){
+		printf("Usage: snowcast_server tcpport [file1] [file2] [file3] [...] \n");
+		exit(-1);
+	}
+
+	char* server_port = argv[1];
+	int sockfd;
+	struct addrinfo hints, *servinfo;
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	
+	int rv;
+	if((rv = getaddrinfo(NULL, server_port, &hints, &servinfo)) == -1){
+		printf("Error in getting %s\n", strerror(errno));
+		exit(-1);
+	}
+	
+	if((sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol)) == -1){
+		close(sockfd);
+		printf("Error in initializing socket: %s\n", strerror(errno));
+		exit(-1);
+	}	
+	
+	if((rv = bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen)) < 0){
+		close(sockfd);
+		printf("Error in binding address to socket: %s\n", strerror(errno));
+		exit(-1);
+	}
+	
+	//create a single thread for listening tcp mesasges...
+	pthread_t listening_thread;
+	pthread_attr_t attr;
+	pthread_attr_init(&attr);
+	pthread_attr_setstacksize(&attr, 20 * 1024 * 1024);
+	pthread_create(&listening_thread, &attr, listening_thread_func, 0);
+	pthread_join(listening_thread, 0);
+	
+
 	return 0;
 }
